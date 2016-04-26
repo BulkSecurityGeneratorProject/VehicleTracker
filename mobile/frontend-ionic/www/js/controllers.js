6ed823a80333ef42cd10b0242994e4d9a85d08fe
alexbruckner <bruckner.alex@gmail.com>
2016-04-26 21:06:53 +0200

    ng-click methods implemented for Send buttons (console log only)

--- mobile/frontend-ionic/www/js/controllers.js
+++ mobile/frontend-ionic/www/js/controllers.js
@@ -6,6 +6,13 @@ angular.module('starter.controllers', [])
 
   $scope.host = $ENV.settings.API.host;
   $scope.port = $ENV.settings.API.port;
+  $scope.vehicle = "BMW123";
+  $scope.driver = "Karl Meerkatz";
+  $scope.order = "ORD000001";
+  $scope.fromLatitude = "48.4105749";
+  $scope.fromLongitude = "15.5977561";
+  $scope.toLatitude = "48.2209603";
+  $scope.toLongitude = "16.3815945";
 
   var id = window.localStorage.getItem('deviceId');
   if (!id) {
@@ -27,6 +34,7 @@ angular.module('starter.controllers', [])
   function updateTime () {
     var now = new Date().getTime();
     $scope.time = $filter('date')(now, "yyyy-MM-dd HH:mm:ss");
+    $scope.deadline = $filter('date')(now, "yyyy-MM-dd HH:mm:ss");
     $timeout(updateTime, 1000);
   }
 
@@ -49,5 +57,44 @@ angular.module('starter.controllers', [])
   };
 
   $scope.checkGPS();
+
+  //--- form defaults and actions ---//
+
+  $scope.sendStammdaten = function() {
+    var vehicle = getValue("vehicle");
+    var driver = getValue("driver");
+    console.log($scope.deviceId);
+    console.log(vehicle);
+    console.log(driver);
+  };
+
+  $scope.sendOrder = function() {
+    var order = getValue("order");
+    var fromLatitude = getValue("fromLatitude");
+    var fromLongitude = getValue("fromLongitude");
+    var toLatitude = getValue("toLatitude");
+    var toLongitude = getValue("toLongitude");
+    var deadline = getValue("deadline");
+    console.log($scope.deviceId);
+    console.log(order);
+    console.log(fromLatitude);
+    console.log(fromLongitude);
+    console.log(toLatitude);
+    console.log(toLongitude);
+    console.log(deadline);
+  };
+
+  $scope.sendLocation = function() {
+    console.log($scope.deviceId);
+    console.log($scope.position.longitude);
+    console.log($scope.position.latitude);
+    console.log($scope.time);
+  };
+
+  function getValue(field) {
+    var value = document.getElementById(field).value;
+    if (!value) value = $scope[field];
+    return value;
+  }
 
 });
